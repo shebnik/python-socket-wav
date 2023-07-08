0d0b2d5d58ff8e20ac7e43f6f1eb9744763eee8c
Nikita Shebalin <shebalin13@gmail.com>
2023-07-08 20:34:02 +0300

    header approach

--- client.py
+++ client.py
@@ -3,58 +3,51 @@ import socket
 import pyaudio
 import re
 import threading
+import struct
 
 # Create a socket and connect to the server
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('localhost', 8000))
 
-# Receive the initial buffer size, sample rate, and channels from the server
-params = sock.recv(1024).decode()
-if params.startswith('settings:'):
-    settings = params.split(':')
-    buffer_size = int(settings[1])
-    sample_rate = int(settings[2])
-    channels = int(settings[3])
-else:
-    # Handle error or default values
-    buffer_size = 1024
-    sample_rate = 44100
-    channels = 1
+HEADER_FORMAT = "!IIII"  # Four unsigned integers, 8 bytes (64 bits)
+
+# Receive the header data
+header_data = sock.recv(16)
+
+# Unpack the header
+buffer_size, sample_rate, channels, sample_width = struct.unpack(HEADER_FORMAT, header_data)
 
 # Initialize PyAudio
 p = pyaudio.PyAudio()
 
 # Open a new audio stream
 stream = p.open(
-    format=p.get_format_from_width(2),
+    format=p.get_format_from_width(sample_width),
     channels=channels,
     rate=sample_rate,
     output=True
 )
-print(f'Stream started with buffer size: {buffer_size}, sample rate: {sample_rate}, channels: {channels}')
+print(f'Stream started with buffer size: {buffer_size}, sample rate: {sample_rate}, channels: {channels}, sample width: {sample_width}')
 # Receive and play the audio data
 data = sock.recv(buffer_size)
 while data:
-    if (b'settings:') in data:
-        print('Received settings update.')
-        encoded_settings = data[data.index(b'settings:'):data.index(b':settings')]
-        settings = encoded_settings.decode().split(':')
-        buffer_size = int(settings[1])
-        sample_rate = int(settings[2])
-        channels = int(settings[3])
-        # Update the audio stream parameters
-        stream.stop_stream()
-        stream.close()
-        stream = p.open(
-            format=p.get_format_from_width(2),
-            channels=channels,
-            rate=sample_rate,
-            output=True
-        )
-        print(f'Updated to buffer size: {buffer_size}, sample rate: {sample_rate}, channels: {channels}')
-        
-        # remove settings from data
-        data = data.replace(encoded_settings, b'')
+    # if 
+    #     print('Received settings update.')
+    #     buffer_size =
+    #     sample_rate =
+    #     channels =
+    #     sample_width =
+    #
+    #     # Update the audio stream parameters
+    #     stream.stop_stream()
+    #     stream.close()
+    #     stream = p.open(
+    #         format=p.get_format_from_width(sample_width),
+    #         channels=channels,
+    #         rate=sample_rate,
+    #         output=True
+    #     )
+    #     print(f'Updated to buffer size: {buffer_size}, sample rate: {sample_rate}, channels: {channels}, sample width: {sample_width}')
         
     stream.write(data)
     data = sock.recv(buffer_size)
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import wave
 import socket
 import threading
+import struct
 
-# Configure the initial buffer size and sample rate
-BUFFER_SIZE = 128
-SAMPLE_RATE = 44100
+# Configure the initial buffer size
+BUFFER_SIZE = 256
 
 # Create a socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,49 +12,32 @@ sock.bind(('localhost', 8000))
 sock.listen(1)
 print('Listening on port 8000...')
 
+# Define the header structure
+HEADER_FORMAT = "!IIII"
+
 # Accept a client connection
 conn, addr = sock.accept()
 print('Connected by', addr)
 
 # Open the wave file
 with wave.open('audio.wav', 'rb') as audio_file:
-    # Get the audio file parameters
+    # Get the audio file parameters    
+    sample_rate = audio_file.getframerate()
     channels = audio_file.getnchannels()
     sample_width = audio_file.getsampwidth()
+    
+    # Send the header data to the client
+    header_data = struct.pack(HEADER_FORMAT, BUFFER_SIZE, sample_rate, channels, sample_width)
+    print('Sending initial header data:', header_data)
+    conn.sendall(header_data)
 
-    # Send the initial buffer size, sample rate, and channels to the client
-    params = f':{BUFFER_SIZE}:{SAMPLE_RATE}:{channels}:'.encode()
-    conn.send(params)
-
-    # Function to handle user input
-    def handle_user_input():
-        global BUFFER_SIZE, SAMPLE_RATE
-        while True:
-            command = input('Enter command (buffer, rate): ')
-            if command == 'buffer':
-                BUFFER_SIZE = int(input('Enter new buffer size: '))
-                print('Buffer size updated.')
-                # Send control message to client
-                control_message = f'settings:{BUFFER_SIZE}:{SAMPLE_RATE}:{channels}:settings'.encode()
-                conn.send(control_message)
-            elif command == 'rate':
-                SAMPLE_RATE = int(input('Enter new sample rate: '))
-                print('Sample rate updated.')
-                # Send control message to client
-                control_message = f'settings:{BUFFER_SIZE}:{SAMPLE_RATE}:{channels}:settings'.encode()
-                conn.send(control_message)
-            else:
-                print('Invalid command.')
-
-    # Start a thread to handle user input
-    user_input_thread = threading.Thread(target=handle_user_input)
-    user_input_thread.daemon = True
-    user_input_thread.start()
+    # Function to handle user input of buffer size or sample rate
+    
 
     # Read and stream the audio data
     data = audio_file.readframes(BUFFER_SIZE)
     while data:
-        conn.send(data)
+        conn.sendall(data)
         data = audio_file.readframes(BUFFER_SIZE)
 
 # Close the connection
