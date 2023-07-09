bdb0a0e65ff998272dccb087bfdddb4495125350
Nikita Shebalin <shebalin13@gmail.com>
2023-07-09 14:39:03 +0300

    possible error handling

--- client.py
+++ client.py
@@ -34,11 +34,27 @@ print(f'Stream started with buffer size: {buffer_size}, sample rate: {sample_rat
 i = 0
 data = sock.recv(buffer_size * 4 + 16)
 while data:
+    # # DEBUG
     # timestamp = time.time()
     # formatted_time = datetime.fromtimestamp(timestamp).strftime('%H:%M:%S:%f')
     # print(str(formatted_time) + ' - ' + str(i))
+    # with open('output.txt', 'a') as f:
+    #     f.write(str(i)  + f': {buffer_size} - ' + str(data)+ '\n')
     
-    _buffer_size, _sample_rate, _channels, _sample_width = struct.unpack(HEADER_FORMAT, data[:16])
+    # with open('header.txt', 'a') as f:
+    #     f.write(str(data[:16]) + '\n')
+
+    _buffer_size = buffer_size
+    _sample_rate = sample_rate
+    _channels = channels
+    _sample_width = sample_width
+    
+    try:
+        _buffer_size, _sample_rate, _channels, _sample_width = struct.unpack(HEADER_FORMAT, data[:16])
+    except:
+        print('Error unpacking header')
+        print(str(i) + ': ' + str(data))        
+        
     if _buffer_size != buffer_size:
         print(f'Buffer size changed from {buffer_size} to {_buffer_size}')
         buffer_size = _buffer_size
@@ -82,12 +98,14 @@ while data:
             output=True
         )
     
+    # Write the audio data to the stream
     data = data[16:]
     stream.write(data)
     
     # Send acknowledgment to the server
     sock.send(b'A')
     
+    # Receive the next chunk of audio data
     data = sock.recv(buffer_size * 4 + 16)
     i += 1
 
